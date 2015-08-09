5c5c73fb0f174c28111c44ac75ed4921134b5b74
stefano-m1 <stefano.meschiari@gmail.com>
2015-08-09 11:44:24 -0500

    Small fixes

--- plot1.R
+++ plot1.R
@@ -1,9 +1,11 @@
 library(readr)
 
-data <- read_csv('data.csv')
+# Read in data filtered to by only keeping rows within dates
+# [2007-02-01:2007-02-02]
+data <- read_csv('filtered_data.csv')
 
 png('plot1.png')
-hist(data$Global_active_power, xlab='Global Active Power',
+hist(data$Global_active_power, xlab='Global Active Power (kilowatts)',
      main='Global Active Power', col='red')
 dev.off()
 
--- plot2.R
+++ plot2.R
@@ -3,7 +3,7 @@ library(lubridate)
 
 # Read in data filtered to by only keeping rows within dates
 # [2007-02-01:2007-02-02]
-data <- read_csv('data.csv')
+data <- read_csv('filtered_data.csv')
 
 # Convert date & time to datetime
 date <- ymd_hms(paste(data$Date, data$Time))
